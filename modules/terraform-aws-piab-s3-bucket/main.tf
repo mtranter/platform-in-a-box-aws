7814002241cb15b36b5f2ecebb290735458e3796
Mark Tranter <mark.a.tranter@gmail.com>
2023-01-30 21:16:50 +1100

    Fix

--- modules/terraform-aws-piab-s3-bucket/main.tf
+++ modules/terraform-aws-piab-s3-bucket/main.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.key.arn
+      kms_master_key_id = aws_kms_key.key[0].arn
       sse_algorithm     = "aws:kms"
     }
   }
